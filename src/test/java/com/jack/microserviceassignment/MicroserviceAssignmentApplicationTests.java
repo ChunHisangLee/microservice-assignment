4b3890fc582ec942f0156b969455b730052565da
Jack Lee <goldrushkl168@gmail.com>
2024-09-12 23:58:29 +0800

    2024/09/12 init

---
+++ src/test/java/com/jack/microserviceassignment/MicroserviceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.microserviceassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
