cc402d89061de2d43c9d76229210b593f3e432f9
ChunHisangLee <goldrushkl168@gmail.com>
2024-09-16 11:27:37 +0800

    2024/09/16  init outbox-service

---
+++ outbox-service/src/main/java/com/jack/outboxservice/OutboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jack.outboxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutboxServiceApplication.class, args);
+    }
+
+}
