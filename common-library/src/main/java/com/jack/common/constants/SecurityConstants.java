9e1584a725e429588633f4ace2b3488f5203e0ef
Jack Lee <goldrushkl168@gmail.com>
2024-09-17 09:55:02 +0800

    2024/09/15 construct common-library

--- common-library/src/main/java/com/jack/common/constants/SecurityConstants.java
+++ common-library/src/main/java/com/jack/common/constants/SecurityConstants.java
@@ -1,25 +1,47 @@
 package com.jack.common.constants;
 
 public class SecurityConstants {
+
     private SecurityConstants() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 
+    // Constants for headers and tokens
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
-    private static final String[] PUBLIC_URLS = {
-            "/api/auth/**",
-            "/public/**",
-            "/h2-console/**",
-            "/swagger-ui.html",
-            "/swagger-resources/**",
-            "/v3/api-docs/**",
-            "/swagger-ui/**",
-            "/swagger-ui.html",
-            "/"
-    };
+
+    // Enum for public URLs
+    public enum PublicUrls {
+        API_AUTH("/api/auth/**"),
+        PUBLIC("/public/**"),
+        H2_CONSOLE("/h2-console/**"),
+        SWAGGER_UI("/swagger-ui.html"),
+        SWAGGER_RESOURCES("/swagger-resources/**"),
+        API_DOCS("/v3/api-docs/**"),
+        SWAGGER_UI_PATH("/swagger-ui/**"),
+        ROOT("/");
+
+        private final String url;
+
+        PublicUrls(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
 
     public static String[] getPublicUrls() {
-        return PUBLIC_URLS.clone();
+        return new String[]{
+                PublicUrls.API_AUTH.getUrl(),
+                PublicUrls.PUBLIC.getUrl(),
+                PublicUrls.H2_CONSOLE.getUrl(),
+                PublicUrls.SWAGGER_UI.getUrl(),
+                PublicUrls.SWAGGER_RESOURCES.getUrl(),
+                PublicUrls.API_DOCS.getUrl(),
+                PublicUrls.SWAGGER_UI_PATH.getUrl(),
+                PublicUrls.ROOT.getUrl()
+        };
     }
 }
